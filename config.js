f35a20463fe9b5e57c3030e97b7fe882fc386971
kashafsaeed7842@gmail.com <kashafsaeed7842@gmail.com.noreply.github.com>
2025-12-09 17:42:00 +0500

    commit

---
+++ config.js
@@ -0,0 +1,7 @@
+import { createClient} from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://your-supabase-url.supabase.co'
+const supabaseKey = 'your-anon-or-service-role-key'
+export const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
